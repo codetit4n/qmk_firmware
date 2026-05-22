6d45ec4f621dd04eeeb73386496ed0a305e367ef
Lokesh Kumar [codetit4n] <hi@loke.sh>
2026-05-22 13:04:31 +0530

    keymaps: home and end, crab anim

--- keyboards/lily58/keymaps/codetit4n/keymap.c
+++ keyboards/lily58/keymaps/codetit4n/keymap.c
@@ -6,7 +6,7 @@
 #    define ANIM_RENDER_WPM true
 #    define FAST_TYPE_WPM 45 // Switch to fast animation when over WPM
 
-#    include "music-bars.c"
+#    include "crab.c"
 #endif
 
 enum layer_number {
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-        KC_TAB,   XXXXXXX, KC_S,    XXXXXXX, XXXXXXX, XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_DQUO,
+        KC_TAB,   XXXXXXX, KC_S,    XXXXXXX, XXXXXXX, XXXXXXX,            KC_HOME, KC_HOME, KC_END,   KC_END, KC_COLN, KC_DQUO,
         KC_LSFT,  XXXXXXX, XXXXXXX, KC_C,    XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
         KC_LALT,  KC_LCTL, KC_LGUI, KC_SPC,  KC_ENT,  MO(_LOWER), XXXXXXX,      KC_RALT
     ),
